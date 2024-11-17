75c18c3bed0ac4fe80a0b7d3ff2b8485c93fede2
kurunukoleh <85008916+kurunukoleh@users.noreply.github.com>
2024-11-17 19:59:24 +0200

    lesson5(hw)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datamanager import DBMmanager
 db_manager = DBMmanager("kakut.db")
 db_manager.create_tables()
 
-#db_manager.addd_quiz(4 , "квіз про нігерйв йоу" , "еукуайцсцс")
 #print(db_manager.get_quises())
-#db_manager.addd_question(1 , 2 , "fvaagrgr")
+#db_manager.addd_quiz(6 , "квіз по гимну" , "хо ниравилно ивдповисть на вси  питання той хадер")
+#db_manager.addd_question(15 , 6 , "ха відповиси а це питтаана правилно і получи гимна кусок")
 db_manager.get_question(2)
